b53309cd3ba8268af7c26f94be3f743649462602
Aleksandrs Jegorovs <aleksandrs.jegorovs2@mail.dcu.ie>
2023-05-30 10:21:46 +0100

    index

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -15,11 +16,57 @@ const DUMMY_MEETUPS = [
         address: "an address 2",
         description: "bla bla bla bla"
     }
-]
-function HomePage() {
+];
+
+async function myFetch() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(DUMMY_MEETUPS);
+        }, 2000);
+    });
+};
+
+function HomePage(props) {
+    // const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    // useEffect(() => {
+    //     const getMeetups = async () => {
+    //         const meetups = await myFetch();
+    //         setLoadedMeetups(meetups);
+    //     };
+    //     getMeetups();
+    // }, []);
+
+    // return (
+    //     <MeetupList meetups={loadedMeetups} />
+    // );
     return (
-        <MeetupList meetups={DUMMY_MEETUPS} />
+        <MeetupList meetups={props.meetups} />
     );
 }
-
+//never runs on the client! user credentials can be used
+//runs always(with every request) on the server after deployment
+// export async function getServerSideProps(context) {
+//     //we have access to request and response
+//     const req = context.req;
+//     const res = context.res;
+//     //fetch data from an API
+//     return {
+//         props: {
+//             meetups: await myFetch()
+//         }
+//     }
+// };
+// //never runs on the client! user credentials can be used
+export async function getStaticProps() {
+    //fetch data from an API
+    return {
+        //props received by the functional component(HomePage)
+        props: {
+            meetups: await myFetch()
+        },
+        //ensure the data in not longer than 10 seconds old
+        revalidate: 20 //number of seconds to wait until page regeneration on the server
+    };
+}
 export default HomePage;
